0df34f5cd25f8bd05110e3215d10bb37638e0b42
Alex Kim <alexk2k05@gmail.com>
2022-10-30 18:28:34 -0400

    Created a program to add two numbers

---
+++ main.py
@@ -0,0 +1,9 @@
+def add_nums(num1, num2):
+    return num1+num2
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    num1 = int(input('Input a number: '))
+    num2 = int(input('Input another number: '))
+    print(f'The sum of your numbers is: {add_nums(num1,num2)}')
