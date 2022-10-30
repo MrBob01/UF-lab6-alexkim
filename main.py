ed38c3d979def2f963382beec531c53720e3ecad
Alex Kim <alexk2k05@gmail.com>
2022-10-30 18:41:52 -0400

    Added a method to subtract numbers Added functionality for user to select between adding and subtracting Added option to exit program

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 def add_nums(num1, num2):
     return num1+num2
 
+def subtract_nums(num1, num2):
+    return num1-num2
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    num1 = int(input('Input a number: '))
-    num2 = int(input('Input another number: '))
-    print(f'The sum of your numbers is: {add_nums(num1,num2)}')
+    done = False
+    menu = 'Options menu\n' \
+           '-----------------------\n' \
+           '1. Add two numbers\n' \
+           '2. Subtract two numbers\n' \
+           '3. Exit\n'
+
+    valid_choices = [1,2,3]
+
+    while not done:
+        print(menu)
+        valid = False
+        while not valid:
+            choice = int(input('Please select an option: '))
+            if choice not in valid_choices:
+                print('Invalid choice.\n')
+                continue
+            else:
+                valid = True
+        if choice == 1:
+            num1 = int(input('Input a number: '))
+            num2 = int(input('Input another number: '))
+            print(f'The sum of your numbers is: {add_nums(num1, num2)}')
+            print()
+        elif choice == 2:
+            num1 = int(input('Input a number: '))
+            num2 = int(input('Input another number: '))
+            print(f'The difference of your numbers is: {subtract_nums(num1, num2)}')
+            print()
+        elif choice == 3:
+            done = True
+
